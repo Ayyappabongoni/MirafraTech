79a08d089ff983a5fc124ed68c9b975bb8f9566c
Ayyappa Bongoni <ayyappagoud92@gmail.com>
2023-03-27 13:40:55 +0530

    pratice

---
+++ pratice/prime_bitwise.c
@@ -0,0 +1,14 @@
+//prime no 
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("emter the number\n");
+	scanf("%d",&a);
+	if((a & a-1)==a-1)
+	{
+		printf("%d is prime no\n",a);
+	}
+	else
+		printf("%d is not a prime number\n",a);
+}
