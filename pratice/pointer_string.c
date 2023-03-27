d7d6f334a7068b98b98bdeba88c9c962d0a67fd6
Ayyappa Bongoni <ayyappagoud92@gmail.com>
2023-03-27 15:06:56 +0530

    pratice

---
+++ pratice/pointer_string.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*char *p="hello";//p store in .data and hello is .rodata
+int main()
+{
+*p='w';//value connot be changed but address can
+//segementation fault
+printf("%s\n",p);
+}*/
+
+
+/*
+int main()
+{
+char *p="world"; //p is stored in stack and world is .rodata
+
+*p='o';//value cannot be modified but address can
+//segmentation fault
+printf("%s\n",p);
+}*/
+
+
+
+/*int main()
+{
+char s[10]="monday";
+s[1]='s';//can be modified
+
+printf("%s\n",s);
+}*/
