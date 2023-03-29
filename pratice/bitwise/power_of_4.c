c591ee35a913e1833ae0c7b2e0ec406240a86710
Ayyappa Bongoni <ayyappagoud92@gmail.com>
2023-03-29 15:34:05 +0530

    bitwise

--- pratice/bitwise/power_of_4.c
+++ pratice/bitwise/power_of_4.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int a=2;
-while(a!=1){
-if(a(a<<1)==0)
-c++;
+	int a,c=0;
+	printf("enter the value\n");
+	scanf("%d",&a);
+	if((a&(a-1))==0)
+	{
+		while((a&1)==0)
+		{
+			c++;
+			a=a>>1;
+		}
+	}
+	if(c%2==0)
+	{
+		printf("power\n");
+	}
+	else
+		printf("not power\n");
 }
-}
+
